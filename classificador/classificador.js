34b21c5bb10786d5942b34e31541f2da6a00c62c
Shahdaddana <shahdaddana@yahoo.com>
2024-05-07 05:02:44 -0300

    a

--- classificador/classificador.js
+++ classificador/classificador.js
@@ -30,68 +30,85 @@ function lerFrases(destino) {
      })
 }
 
-/*
+async function gerarFrase(idTextoGrande, idTextoPequeno){
+    const textoGrande = document.getElementById(idTextoGrande)
+    const textoPequeno = document.getElementById(idTextoPequeno)
+    const amount = '1'
+    const category = ''
+    const difficulty = ''
+    const type = 'boolean'
+    const apiUrl = `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}`
 
-async function getAuthSheets(){
-    const auth = new GoogleAuth({
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets"
-    })
-    const client = await auth.getClient()
-    const googleSheets = google.sheets({
-        version: "v4",
-        auth: client
-    })
-    const spreadsheetId = "1Ig5zFv6Djkn8lTQrCiy6qql-kk9FWiOWgRsarNF3ggI"
-    return { client }
-}
+    // Amount: 1-10
+    // Difficulty: easy, medium, hard
+    // Type: multiple, boolean
+    /* Category:
+    9 General Knowledge
+    10 Entertainment: Books
+    11 Entertainment: Film
+    12 Entertainment: Music
+    13 Entertainment: Musicals & Theatres
+    14 Entertainment: Television
+    15 Entertainment: Video Games
+    16 Entertainment: Board Games
+    17 Science & Nature
+    18 Science: Computers
+    19 Science: Mathematics
+    20 Mythology
+    21 Sports
+    22 Geography
+    23 History
+    24 Politics
+    25 Art
+    26 Celebrities
+    27 Animals
+    28 Vehicles
+    29 Entertainment: Comics
+    30 Science: Gadgets
+    31 Entertainment: Japanese Anime & Manga
+    32 Entertainment: Cartoon & Animations */
 
-async function getRows(){
-
-    const { googleSheets, auth, spreadsheetId } = await getAuthSheets()
-
-    const getRows = await googleSheets.spreadsheets.value({
-        auth,
-        spreadsheetId,
-        range: "pagina"
-    })
-}
-
-
-
-
-const apiKey = "credentials.json"
-const spreadsheetId = '1Ig5zFv6Djkn8lTQrCiy6qql-kk9FWiOWgRsarNF3ggI'
-const range = 'pagina'
-
-function testar() {
-    gapi.load('client', initClient);
-}
-
-
-
-function initClient() {
-    gapi.client.init({
-        'apiKey': apiKey,
-        'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4']
-    }).then(function() {
-        return gapi.client.sheets.spreadsheets.values.get({
-            spreadsheetId: spreadsheetId,
-            range: range,
-        })
-    }).then(function(response) {
-        const rangeData = response.result.values;
-        if (rangeData.length > 0) {
-            console.log('Dados obtidos:');
-            rangeData.forEach(row => {
-                console.log(`${row[0]}, ${row[1]}`)
-            })
-        } else {
-            console.log('Nenhum dado encontrado.')
+    fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Erro ao obter pergunta')
         }
-    }, function(error) {
-        console.error('Erro ao ler os dados:', error)
+        return response.json()
+    })
+    .then(data => {
+        const pergunta = data.results[0].question
+        //const category = data.results[0].category
+        //const difficulty = data.results[0].difficulty
+        const respostaCerta = data.results[0].correct_answer
+        //const incorrectAnswers = data.results[0].incorrect_answers
+
+        // Exibe a pergunta e outras informações
+        //console.log('Categoria:', category);
+        //console.log('Dificuldade:', difficulty);
+        //console.log('Pergunta:', question);
+        //console.log('Resposta correta:', correctAnswer);
+        //console.log('Respostas incorretas:', incorrectAnswers);
+        //console.log(data.results[0].question)
+        textoGrande.innerHTML = pergunta
+        textoPequeno.innerText = respostaCerta
+        const caixaPequena = document.getElementById('idCaixaPequena')
+        caixaPequena.classList.add(respostaCerta);
+
+        
+
+
+
+    })
+    .catch(error => {
+        console.error('Erro ao obter pergunta:', error);
     })
 }
 
-*/
+async function eventoToogle(idElemento) {
+    toggleSwitch.addEventListener('change', () => {
+        const toogle = document.getElementById(idElemento)
+        const isChecked = toogle.checked
+        const newLanguage = isChecked ? 'pt' : 'en'
+        console.log(newLanguage)
+    })
+}
