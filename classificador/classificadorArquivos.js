9192930bc65c0b447f2f794ae84f8ad6e6302b1d
Shahdaddana <shahdaddana@yahoo.com>
2024-05-12 04:20:43 -0300

    a

--- classificador/classificadorArquivos.js
+++ classificador/classificadorArquivos.js
@@ -1,3 +1,7 @@
+const codigo = 'AKfycbwaECTDwCt2_QJ7TaFxNp5VvtKCqcbAZQHhFS1hxEoBNYICwU-ryrpE9GfdM2t0247hgA'
+const urlPlanilha = 'https://script.google.com/macros/s/'+codigo+'/exec'
+
+
 /**
  * @param {*} elementoFonte Página ou conteúdo em HTML a ser copiado
  * @param {*} elementeDestino Id da div onde o conteúdo será carregado
@@ -12,36 +16,6 @@ function carregarElemento(elementoFonte, elementeDestino){
      })
 }
 
-async function lerPanilha(idTextoGrande, idTextoPequeno){
-    const textoGrande = document.getElementById(idTextoGrande)
-    const textoPequeno = document.getElementById(idTextoPequeno)
-    const amount = '1'
-    const category = ''
-    const difficulty = ''
-    const type = 'boolean'
-    const apiUrl = `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}`
-
-    fetch(apiUrl)
-    .then(response => {
-        if (!response.ok) {
-        throw new Error('Erro ao obter pergunta')
-        }
-        return response.json()
-    })
-    .then(data => {
-        const pergunta = data.results[0].question
-        const respostaCerta = data.results[0].correct_answer
-        
-        textoGrande.innerHTML = pergunta
-        textoPequeno.innerText = respostaCerta
-        const caixaPequena = document.getElementById('idCaixaPequena')
-        caixaPequena.classList.add(respostaCerta)
-    })
-    .catch(error => {
-        console.error('Erro ao obter pergunta:', error);
-    })
-}
-
 async function eventoToogle(idElemento) {
     toggleSwitch.addEventListener('change', () => {
         const toogle = document.getElementById(idElemento)
@@ -51,54 +25,107 @@ async function eventoToogle(idElemento) {
     })
 }
 
-async function receber(){
-    const url = 'https://script.google.com/macros/s/AKfycbzQ9_15V49dB9G0fh2tu8hglBw780gKycTqmuTkftq2JhWAvU5D3kGEl9KF1SiLiKJQbg/exec'
-
-    fetch(url)
-    .then(response => {
-        if (!response.ok) {
-        throw new Error('Erro ao obter pergunta')
-        }
-        return response.json()
-    })
-    .then(data => {
-        console.log(data.saida)
-        return "ola"
-        const pergunta = data.results[0].question
-        const respostaCerta = data.results[0].correct_answer
-        
-        textoGrande.innerHTML = pergunta
-        textoPequeno.innerText = respostaCerta
-        const caixaPequena = document.getElementById('idCaixaPequena')
-        caixaPequena.classList.add(respostaCerta)
-    })
-    .catch(error => {
-        console.error('Erro ao obter pergunta:', error);
-    })
-    
-}
-
-async function planilhaPost(mensagem) {
-    const url = 'https://script.google.com/macros/s/AKfycby11FAooFaA98cw_LlwtASLJeKkD_XgUMe69_WJfaZEK7qBuFoFdCCQ84uyvZDyZIfLbg/exec';
-    const dados = {
-      valor: mensagem
-    }
-  
-    try {
-      const resposta = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(dados)
-      });
-  
-      if (resposta.ok) {
-        console.log('Requisição POST enviada com sucesso!');
-      } else {
-        console.error('Erro ao enviar requisição POST:', resposta.status);
+// Atualiza as variaveis locais com os dados da ultima linha da planilha
+async function obterComentarios() { // GET
+  try {
+      const response = await fetch(urlPlanilha)
+      if (!response.ok) {
+          throw new Error('Erro ao receber texto')
       }
-    } catch (erro) {
-      console.error('Erro ao enviar requisição POST:', erro);
-    }
+      const data = await response.json()
+      return data.saida
+  } catch (error) {
+      console.error('Erro ao receber texto', error)
+      return []
+  }
 }
+
+// Funcao genérica de POST
+async function post(dados) {
+  try {
+      const resposta = await fetch(urlPlanilha, {
+          redirect: "follow",
+          method: 'POST',
+          body: JSON.stringify(dados),
+          headers: {
+              'Content-Type': 'text/html',
+              'charset': 'UTF-8'
+          },
+      })
+      
+      if (resposta.ok) {
+          console.log('Requisição POST enviada com sucesso');
+      } else {
+          console.error('Erro ao enviar requisição POST', resposta.status)
+      }
+  } catch (erro) {
+      console.error('Erro ao enviar requisição POST', erro)
+  }
+}
+
+async function postarComentario(comentario) { // POST
+  const dados = {
+    funcao: 'postarComentario',
+    valor: comentario,
+  }
+  post(dados)
+}
+
+async function aprovarComentario(id, valor) { // POST
+  console.log(id, valor)
+  const dados = {
+    funcao: 'aprovarComentario',
+    id,
+    valor,
+  }
+  post(dados)
+}
+
+function eventoBotao(idBotao, idComentario){
+  let aprovado = 'TRUE'
+  if (idBotao == 'botaoRejeitar'){
+    aprovado = 'FALSE'
+  }
+  const botao = document.getElementById(idBotao)
+
+  botao.addEventListener('click', function() {
+    aprovarComentario(idComentario, aprovado)
+  })
+}
+
+///////////////////
+function criarCards(comentarios) {
+  const cardContainer = document.getElementById('card-container')
+
+  // Limpa o conteúdo existente no contêiner de cartões
+  cardContainer.innerHTML = ''
+
+  // Itera sobre os comentários e cria um card para cada um
+  comentarios.forEach(comentario => {
+    const col = document.createElement('div')
+    col.classList.add('col-md-4', 'mb-4')
+
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    // Estilize o card com base no valor do comentário
+    if (comentario[2] == false){
+      card.classList.add('rejeitado')
+    }
+    card.classList.add('aprovado')
+    
+
+    const cardBody = document.createElement('div')
+    cardBody.classList.add('card-body')
+    cardBody.style.writingMode = "horizontal-tb"
+
+    // Coloca o comentário no Card
+    cardBody.textContent = comentario[1]
+
+    card.appendChild(cardBody)
+    col.appendChild(card)
+    cardContainer.appendChild(col)
+  })
+}
+
+
