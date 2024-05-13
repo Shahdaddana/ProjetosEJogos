0d4a7a90016a79b60d13655fd76a778778a72f4f
Shahdaddana <shahdaddana@yahoo.com>
2024-05-13 10:36:11 -0300

    a

---
+++ index.js
@@ -0,0 +1,86 @@
+function botaoBLIP(){
+    new BlipChat()
+        .withAppKey('ZnVuY29lc2Vqb2dvczo5ZjgyNDA2OS1lZDNkLTQwZmMtYTZkNi0yMzkxNjY0ZjQzYTE=')
+        .withButton({"color":"#0096fa","icon":""})
+        .withCustomCommonUrl('https://shahdad-dana-m34ud.chat.blip.ai/')
+        .build()
+}
+
+function criarCard(titulo, descricao, imagem, href) {
+    const cardHtml = `
+    <div class="col-md-4 col-sm-4">
+        <div class="feature-center animate-box" data-animate-effect="fadeIn">
+            <span class="icon">
+                <img class="iconeRedondo" src="${imagem}">
+            </span>
+            <h3>${titulo}</h3>
+            <p>${descricao}</p>
+            <p><a href="${href}" class="btn btn-primary btn-outline">Acessar</a></p>
+        </div>
+    </div>
+    ` 
+    return cardHtml;
+}
+
+function adicionarCards() {
+    const linhaCards = document.getElementById('linhaCards')
+
+    // Nuvem
+    linhaCards.innerHTML += criarCard(
+        "Nuvem de Palavras",
+        "Cria uma nuvem de palavras a partir de um texto",
+        "images/nuvem.png",
+        "nuvem/nuvem.html")
+
+    // Trivia
+    linhaCards.innerHTML += criarCard(
+        "Gerador de Trivia",
+        "Gera perguntas aleatórias sobre trivialidades",
+        "images/geradorTrivia.jpeg",
+        "geradorDeTrivia.html")
+
+    // QR
+    linhaCards.innerHTML += criarCard(
+        "Leitor de Código QR",
+        "Apresenta o valor do código QR, inclusive se for um WIFI",
+        "images/codigoQR.jpeg",
+        "leitorQr.html")
+
+    // Ponteiros
+    linhaCards.innerHTML += criarCard(
+        "Jogo dos Ponteiros",
+        "Jogo desenvolvido para experimentação de conceitos em IA",
+        "images/ponteiroIcone.png",
+        "jogoDosPonteiros.html")
+
+    /*
+    // Envoltória
+    linhaCards.innerHTML += criarCard(
+        "Envoltória Convexa",
+        "Alguns métodos para se calcular uma envoltória convexa",
+        "images/envoltoria.png",
+        "envoltoriaConvexa.html")
+
+    // Classificador
+    linhaCards.innerHTML += criarCard(
+        "Classificador de Comentários",
+        "Categoriza comentários de redes sociais, avalia nível de toxicidade",
+        "images/classificador.jpeg",
+        "classificador/classificadorArquivos.html")
+
+    // Jogos
+    linhaCards.innerHTML += criarCard(
+        "Jogos",
+        "Jogos simples para implementar algoritmos complexos",
+        "images/joystick.jpeg",
+        "#")
+
+    // Projetos
+    linhaCards.innerHTML += criarCard(
+        "Projetos",
+        "Coletânea de mini projetos feitos para experimentação e provas de conceito",
+        "images/prancheta.png",
+        "#")
+    */
+}
+
